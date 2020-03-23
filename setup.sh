59b6b0166bee9e127ff983ced82e9b688631bfa5
SantaKrauze <gracjantokarz38@gmail.com>
2020-03-23 18:01:22 +0100

    dealing with config files

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
 set -e
+echo Deploying dotfiles:
+cp .vimrc ~/
+cp .Xresources ~/
+cp config ~/.config/i3
+cp xorg.conf /etc/X11
+cp Xresources /etc/X11/xdm
+
+echo Setting privilages:
+chmod +x lock.sh
+
 echo Adding rpmfusion:
-dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-echo Installing repos
+dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
+dnf install http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+echo Installing repos:
 echo i3 basics:
 dnf install nodejs-devel i3 i3lock xdm NetworkManager network-manager-applet dbus-devel libX11-devel libXrandr-devel glib2-devel pango-devel gtk2-devel libxdg-basedir-devel libXScrnSaver-devel python-devel cmake 
 
