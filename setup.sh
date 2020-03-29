3b3c1850d8e00677cded2a6dcdc5002b0591e9f5
SantaKrauze <gracjantokarz38@gmail.com>
2020-03-29 18:32:47 +0200

    minor

--- setup.sh
+++ setup.sh
@@ -4,29 +4,29 @@ set -e
 echo Deploying files:
 cp .vimrc ~/
 cp .Xresources ~/
-mkdir ~/.config
-mkdir ~/.config/i3
+mkdir -p ~/.config
+mkdir  -p ~/.config/i3
 cp config ~/.config/i3
 cp xorg.conf /etc/X11
 cp Xresources /etc/X11/xdm
-mkdir ~/Pictures
-mkdir ~/Pictures/bg
+mkdir -p ~/Pictures
+mkdir -p ~/Pictures/bg
 cp icon.png ~/Pictures/bg
 cp 2.png ~/Pictures/bg
 
-echo Setting privilages:
+echo Setting privilages...
 chmod +x lock.sh
 
-echo Adding rpmfusion:
+echo Adding rpmfusion...
 dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
 dnf install http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-echo Installing repos:
+echo Installing repos...
 echo i3 basics:
 dnf install nodejs-devel i3 i3lock xdm NetworkManager network-manager-applet dbus-devel libX11-devel libXrandr-devel glib2-devel pango-devel gtk2-devel libxdg-basedir-devel libXScrnSaver-devel python-devel cmake 
 
 echo Other i3 stuff:
-dnf install dunst xss-lock xbacklight scrot feh lxappearance thunar
+dnf install dunst xss-lock xbacklight feh lxappearance thunar
 
 systemctl enable xdm
 
@@ -35,3 +35,5 @@ dnf install firefox chromium chromium-vaapi vim gvim tlp htop gcc texlive texmak
 
 echo Setting it up:
 feh --bg-fill ~/Pictures/bg/2.png
+
+echo You should reboot now
